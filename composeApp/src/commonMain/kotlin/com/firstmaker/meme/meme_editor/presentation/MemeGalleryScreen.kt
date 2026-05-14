3e90a905d341d351bd8f9e75d31e808e97310d9b
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-05-14 16:56:02 -0400

    added MemeGalleryScreen

--- composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
@@ -2,10 +2,15 @@
 
 package com.firstmaker.meme.meme_editor.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,10 +18,13 @@ import androidx.compose.material3.TopAppBar
 import com.firstmaker.meme.core.presentation.MemeTemplate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.firstmaker.meme.core.presentation.memeTemplates
 import meme.composeapp.generated.resources.Res
 import meme.composeapp.generated.resources.meme_templates
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -45,7 +53,24 @@ fun MemeGalleryScreen(
                 bottom = innerPadding.calculateBottomPadding() + 8.dp,
             )
         ) {
-
+            items(
+                items = memeTemplates,
+                key = { it.id }
+            ) { memeTemplate ->
+                Card(
+                    onClick = {
+                        onMemeTemplateSelected(memeTemplate)
+                    },
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                ) {
+                    Image(
+                        painter = painterResource(memeTemplate.drawable),
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth(),
+                        contentDescription = null
+                    )
+                }
+            }
         }
     }
 }
