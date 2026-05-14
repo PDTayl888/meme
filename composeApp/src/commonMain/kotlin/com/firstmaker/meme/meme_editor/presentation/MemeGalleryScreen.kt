bbed5a1dab69cbdae852ef8a2bb426054f4c5768
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-05-13 23:27:36 -0400

    imported meme template

--- composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
@@ -1,11 +1,12 @@
 package com.firstmaker.meme.meme_editor.presentation
 
+import com.firstmaker.meme.core.presentation.MemeTemplate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MemeGalleryScreen(
-    onMemeTemplateSelected: (memeTemplate) -> Unit
+    onMemeTemplateSelected: (MemeTemplate) -> Unit
 ) {
 
 }
