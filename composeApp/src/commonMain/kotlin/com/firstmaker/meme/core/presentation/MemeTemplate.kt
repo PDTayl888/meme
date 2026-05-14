dee769995ac50ccefc5eb83d384e3a1083bb6eb6
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-05-13 23:25:36 -0400

    added meme template

---
+++ composeApp/src/commonMain/kotlin/com/firstmaker/meme/core/presentation/MemeTemplate.kt
@@ -0,0 +1,19 @@
+package com.firstmaker.meme.core.presentation
+
+import meme.composeapp.generated.resources.Res
+import meme.composeapp.generated.resources.allDrawableResources
+import org.jetbrains.compose.resources.DrawableResource
+
+data class MemeTemplate(
+    val id: String,
+    val drawable: DrawableResource
+)
+
+val memeTemplates = Res.allDrawableResources
+    .filterKeys { it.startsWith("meme_template") }
+    .map { (key, value) ->
+        MemeTemplate(
+            id = key,
+            drawable = value
+        )
+    }
---
+++ composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
@@ -0,0 +1,13 @@
+package com.firstmaker.meme.meme_editor.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MemeGalleryScreen(
+    onMemeTemplateSelected: (memeTemplate) -> Unit
+) {
+
+}
+
+
