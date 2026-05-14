6b35a62cd7cf030e7ac964bb78769eddeec7d923
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-05-14 12:18:26 -0400

    added UI

--- composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/firstmaker/meme/meme_editor/presentation/MemeGalleryScreen.kt
@@ -1,14 +1,53 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.firstmaker.meme.meme_editor.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import com.firstmaker.meme.core.presentation.MemeTemplate
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import meme.composeapp.generated.resources.Res
+import meme.composeapp.generated.resources.meme_templates
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun MemeGalleryScreen(
     onMemeTemplateSelected: (MemeTemplate) -> Unit
 ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(Res.string.meme_templates)
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Adaptive(150.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalItemSpacing = 16.dp,
+            contentPadding = PaddingValues(
+                start = innerPadding.calculateLeftPadding(LayoutDirection.Ltr) + 8.dp,
+                end = innerPadding.calculateRightPadding(LayoutDirection.Ltr) + 8.dp,
+                top = innerPadding.calculateTopPadding() + 8.dp,
+                bottom = innerPadding.calculateBottomPadding() + 8.dp,
+            )
+        ) {
 
+        }
+    }
 }
 
 
